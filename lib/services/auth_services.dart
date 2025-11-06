23619c6bace5bf2cbab5e9688598df3e67a58bd9
Antonius Kaharap Kautsar <antonisback123@gmail.com>
2025-11-07 00:02:50 +0700

    made the api hosted on my computer now. please read the comment under auth_services

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -6,8 +6,13 @@ import 'package:jawarapbl/shared/models/user_model.dart';
 
 class AuthService {
   // change to your local ipv4 address and the port to any unused port
-  String get baseUrl => 'http://192.168.100.14:8000/api';
-  String get storageUrl => 'http://192.168.100.14:8000/storage';
+  // String get baseUrl => 'http://192.168.18.15:8000';
+  // String get storageUrl => 'http://192.168.18.15:8000';
+
+  // this one is the domain im running with localtunnel, change it accordingly if you made changes to the api.
+  // simply comment below and uncomment the above to run on local network
+  String get baseUrl => 'https://jawara-api.loca.lt/api';
+  String get storageUrl => 'https://jawara-api.loca.lt/storage';
 
   Future<String?> login(String email, String password) async {
     try {
